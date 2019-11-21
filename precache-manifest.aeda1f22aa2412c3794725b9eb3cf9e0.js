e351fac91354724e5ed2c382feeaff0c807a3e67
Jimmy <jimmych679@gmail.com>
2019-11-21 17:26:56 -0500

    Updates

---
+++ precache-manifest.aeda1f22aa2412c3794725b9eb3cf9e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242b1b14d49b5ff0c8e9ca1909b950af",
+    "url": "/thonking-emoji/./index.html"
+  },
+  {
+    "revision": "6e02d7b4af8a85a4127e",
+    "url": "/thonking-emoji/./static/css/main.0f2a5808.chunk.css"
+  },
+  {
+    "revision": "e0396d4c5a2fff7ab00f",
+    "url": "/thonking-emoji/./static/js/2.9c9ae1af.chunk.js"
+  },
+  {
+    "revision": "6e02d7b4af8a85a4127e",
+    "url": "/thonking-emoji/./static/js/main.0c28c2d9.chunk.js"
+  },
+  {
+    "revision": "f2948f015c3c1de32160",
+    "url": "/thonking-emoji/./static/js/runtime-main.876cebb5.js"
+  },
+  {
+    "revision": "ea895f26200ae65449b4d3a1b5851d20",
+    "url": "/thonking-emoji/./static/media/background.ea895f26.jpg"
+  },
+  {
+    "revision": "9763868f9a7c08c3564d2d5a91de8ea5",
+    "url": "/thonking-emoji/./static/media/hand.9763868f.png"
+  },
+  {
+    "revision": "76f28cbe53670c78ac40bd3541ef3595",
+    "url": "/thonking-emoji/./static/media/thonking.76f28cbe.png"
+  }
+]);
